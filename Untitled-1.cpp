bba0ec592cfeb0d427207b741840c19f5cbfc6a8
AwesomeUser123Martin <159699179+AwesomeUser123Martin@users.noreply.github.com>
2024-02-17 10:14:29 -0800

    testing numbers

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,9 +1,17 @@
-// Your First C++ Program
+#include<iostream>
+int main ()
+     
+     {
+        using std :: cout ;
+        using std:: cin ;
+        int Number ;
+        cout << "\nEnter a Number less than 10 or Greater 100! : " ;
+        cin >> Number ;
+        if (Number > 10 )
+         {if (Number > 100)
+           cout << " it is Greater Than 100! : " ;
+         }
+         else
+          cout << "it is less Than 10! : " ;
 
-#include <iostream>
-
-int main() {
-    std::cout << "Hello jksdhfsdjkfsjhfsjhdfgsdgufygfyhfvgsdhf!";
-    return 0;
-}
-
+     }
